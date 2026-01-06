b508c89c8638df2a19645037de55b505aa87ca1b
Aliromi21 <aliromiah13@gmail.com>
2026-01-06 05:23:08 +0100

    Solve: Likes system (Codewars)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Problem Solving Practice
+
+This repository contains my solutions to programming challenges
+from platforms like Codewars and LeetCode.
+
+## Languages
+- JavaScript
+
+## Focus
+- Data Structures
+- Algorithms
+- Problem Solving
+
---
+++ codewars/likes-system.js
@@ -0,0 +1,26 @@
+/*
+Problem:
+Given an array of names, return the like text.
+
+Approach:
+- Use switch on array length
+- Handle each case separately
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
